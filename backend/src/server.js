9f5d917be7ee93bb31d5f36b5327e6a19b9f46d5
wslmacieira <wslmacieira@outlook.com>
2019-10-13 22:59:21 -0300

    finished frontend

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,13 +1,19 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
+const path = require("path");
+
 const routes = require("./routes");
 
 const app = express();
 
-mongoose.connect('mongodb+srv://omnistack:omnistack@omnistack9-b4ne1.mongodb.net/aircnc?retryWrites=true&w=majority', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
+mongoose.connect(
+  "mongodb+srv://omnistack:omnistack@omnistack9-b4ne1.mongodb.net/aircnc?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }
+);
 
 // GET, POST, PUT, DELETE
 
@@ -15,8 +21,10 @@ mongoose.connect('mongodb+srv://omnistack:omnistack@omnistack9-b4ne1.mongodb.net
 //req.params = Acessar route params (para edição, delete)
 //req.body = Acessar corpo da requisição (para criação, edição)
 
+app.use(cors());
 app.use(express.json());
-//depois do json() para o express entender 
+//depois do json() para o express entender
+app.use("/files", express.static(path.resolve(__dirname, '..','upload')));
 app.use(routes);
 
 app.listen(3333);
